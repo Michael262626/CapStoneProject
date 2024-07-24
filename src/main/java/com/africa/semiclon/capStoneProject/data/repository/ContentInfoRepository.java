76c914b929278f6557af4e4f7a998050699288e5
michael <dikandumichael@gmail.com>
2024-07-24 17:57:19 +0100

    initial commit

--- src/main/java/com/africa/semiclon/capStoneProject/data/repository/ContentInfoRepository.java
+++
@@ -1,6 +0,0 @@
-package com.africa.semiclon.capStoneProject.data.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentInfoRepository extends JpaRepository<ContentInfo, Long> {
-}
