63724adad9382827a3833076914842f62b049553
michael <dikandumichael@gmail.com>
2024-07-25 13:01:06 +0100

    initial commit

--- src/main/java/com/africa/semiclon/capStoneProject/services/interfaces/AdminService.java
+++ src/main/java/com/africa/semiclon/capStoneProject/services/interfaces/AdminService.java
@@ -1,25 +1,7 @@
 package com.africa.semiclon.capStoneProject.services.interfaces;
 
-import com.africa.semiclon.capStoneProject.dtos.request.*;
-import com.africa.semiclon.capStoneProject.dtos.response.*;
+import com.africa.semiclon.capStoneProject.data.models.Notification;
 
 public interface AdminService {
-
-    ManageUserResponse manageUsers(ManageUsersRequest request);
-
-    ViewWasteResponse viewAllWaste(ViewWasteRequest viewWasteRequest);
-
-    AssignWasteResponse assignWasteToAgent(AssignWasteRequest request);
-
-    WasteReportResponse generateWasteReport(GenerateWasteReportRequest request);
-
-
-    NotificationResponse sendNotificationRequest(NotificationRequest notificationRequest);
-
-    DeleteUserResponse deleteUser(DeleteUserRequest deleteRequest);
-
-    RegisterAgentResponse registerAgent(RegisterAgentRequest registerRequest);
-
-    RegisterWasteResponse registerWasteForSale(RegisterWasteRequest registerWasteRequest);
 
 }
