c89236a33c760b86a466f54eb16cffb8cabbe0a8
michael <dikandumichael@gmail.com>
2024-08-06 11:50:13 +0100

    initial commit

--- src/main/java/com/africa/semiclon/capStoneProject/security/config/SecurityConfig.java
+++ src/main/java/com/africa/semiclon/capStoneProject/security/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.HttpMethod.*;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -39,8 +39,13 @@ public class SecurityConfig {
                 .addFilterBefore(authorizationFilter, CustomUsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(POST,"/api/v1/auth/**").permitAll()
+                        .requestMatchers(POST,"/api/v1/users/register").permitAll()
+                        .requestMatchers(POST,"/api/v1/users/sellWaste").permitAll()
+                        .requestMatchers(PUT,"/api/v1/users/update").permitAll()
                         .requestMatchers("/api/v1/user/admin").hasAnyAuthority("ADMIN", "AGENT")
                         .requestMatchers("/api/payments/**").permitAll()
+                        .requestMatchers(GET,"api/v1/admin/**").permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
--- src/test/java/com/africa/semiclon/capStoneProject/controller/AdminControllerTest.java
+++ src/test/java/com/africa/semiclon/capStoneProject/controller/AdminControllerTest.java
@@ -4,17 +4,23 @@ import com.africa.semiclon.capStoneProject.data.models.User;
 import com.africa.semiclon.capStoneProject.data.repository.UserRepository;
 import com.africa.semiclon.capStoneProject.dtos.request.LoginRequest;
 import com.africa.semiclon.capStoneProject.dtos.request.ManageUsersRequest;
+import com.africa.semiclon.capStoneProject.dtos.request.PaymentRequest;
+import com.africa.semiclon.capStoneProject.dtos.response.ManageUserResponse;
+import com.africa.semiclon.capStoneProject.services.interfaces.AdminService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,15 +32,19 @@ public class AdminControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private AdminService adminService;
+
     @Test
+    @WithMockUser(roles = {"ADMIN"})
     public void manageUsersTest() throws Exception {
-        ManageUsersRequest request = new ManageUsersRequest();
-        request.setAdminId(1L);
-        byte[] content = new ObjectMapper().writeValueAsBytes(request);
-        mockMvc.perform(post("/api/v1/admin/manageUsers")
+        ManageUserResponse response = new ManageUserResponse();
+        Mockito.when(adminService.manageUsers(Mockito.any(ManageUsersRequest.class)))
+                .thenReturn(response);
+        mockMvc.perform(get("/api/v1/admin/manageUsers")
                         .contentType(APPLICATION_JSON)
-                        .content(content))
-                .andExpect(status().isOk())
+                        .content("{\"adminId\": \"300\"}"))
+                .andExpect(status().isCreated())
                 .andDo(print());
 
     }
