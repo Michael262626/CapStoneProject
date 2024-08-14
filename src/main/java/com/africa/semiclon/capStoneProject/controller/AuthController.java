2481e7caf52dc1bf1790d2a3bcd8ef3665d5fa5f
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-14 15:42:27 +0100

    Merge remote-tracking branch 'origin/BanjoSolomon' into BanjoSolomon

--- src/main/java/com/africa/semiclon/capStoneProject/controller/AuthController.java
+++ src/main/java/com/africa/semiclon/capStoneProject/controller/AuthController.java
@@ -6,10 +6,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import static com.africa.semiclon.capStoneProject.security.utils.SecurityUtils.JWT_PREFIX;
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
