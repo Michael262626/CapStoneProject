53f71fd6198874a7a4df3a6fc72cd7e87cb8cb93
banjoSolomon <ayomidebanjo68@gmail.com>
2024-08-15 20:45:08 +0100

    Merge remote-tracking branch 'origin/BanjoSolomon' into BanjoSolomon

--- src/test/java/com/africa/semiclon/capStoneProject/service/UserServiceTest.java
+++ src/test/java/com/africa/semiclon/capStoneProject/service/UserServiceTest.java
@@ -104,7 +104,7 @@ public class UserServiceTest {
         SellWasteRequest sellWasteRequest = new SellWasteRequest();
 //        sellWasteRequest.setUserId(userId);
         sellWasteRequest.setType(Category.PLASTIC);
-        sellWasteRequest.setQuantity("5kg");
+        sellWasteRequest.setQuantity(5);
         return userService.sellWaste(sellWasteRequest);
     }
 
