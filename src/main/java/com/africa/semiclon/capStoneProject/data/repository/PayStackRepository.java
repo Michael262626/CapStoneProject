6aa1215ab2a1aa782d5c55ec877903be1fd48565
Elusoji Timileyin <elusojitimileyin@gmail.com>
2024-08-15 12:07:25 +0100

    working on render deployment errors cause by repositories

--- src/main/java/com/africa/semiclon/capStoneProject/data/repository/PayStackRepository.java
+++ src/main/java/com/africa/semiclon/capStoneProject/data/repository/PayStackRepository.java
@@ -2,6 +2,8 @@ package com.africa.semiclon.capStoneProject.data.repository;
 
 import com.africa.semiclon.capStoneProject.data.models.PaymentPayStack;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PayStackRepository extends JpaRepository<PaymentPayStack, Long> {
 }
